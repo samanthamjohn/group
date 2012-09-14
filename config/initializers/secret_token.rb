f6eb2287fd0a23017eac09e554ace3b28e3fd163
modocache <modocache@gmail.com>
2012-09-14 18:37:15 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Group::Application.config.secret_token = '823bfc9924bea9089fa843ca673d7abed249f634114a8296684ac4d248243c1ebf591e6ffdd3f1f184dedbc7003a9a70ec7ac7cba6140d9bd3ec3c8e7db30ad1'
