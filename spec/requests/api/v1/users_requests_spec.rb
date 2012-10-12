969d166b811c5efab93a4a60803692842238c72f
Samantha John <samantha.m.john@gmail.com>
2012-10-12 18:18:15 -0400

    Fix hanging test. response should be response.status on line 15.

--- spec/requests/api/v1/users_requests_spec.rb
+++ spec/requests/api/v1/users_requests_spec.rb
@@ -8,22 +8,22 @@ describe 'GET api/v1/users.json' do
       response.status.should == 401
     end
   end
-#  context 'client is authenticated' do
-#    before { authenticate_client }
-#    it 'should return a status code of 200' do
-#      get 'api/v1/users.json'
-#      response.should == 200
-#    end
-#    it 'should return a list of users in JSON as specified in the API contract' do
-#      FactoryGirl.create :user, email: 'jake-the-dog@the-land-of.ooo'
-#
-#      get 'api/v1/users.json'
-#      body = JSON.parse(response.body)
-#      body.should have_key 'users'
-#      body['users'].length.should == 1
-#      body['users'].first['email'].should == 'jake-the-dog@the-land-of.ooo'
-#    end
-#  end
+ context 'client is authenticated' do
+   before { authenticate_client }
+   it 'should return a status code of 200' do
+     get 'api/v1/users.json'
+     response.status.should == 200
+   end
+   it 'should return a list of users in JSON as specified in the API contract' do
+     FactoryGirl.create :user, email: 'jake-the-dog@the-land-of.ooo'
+
+     get 'api/v1/users.json'
+     body = JSON.parse(response.body)
+     body.should have_key 'users'
+     body['users'].length.should == 1
+     body['users'].first['email'].should == 'jake-the-dog@the-land-of.ooo'
+   end
+ end
 end
 
 
